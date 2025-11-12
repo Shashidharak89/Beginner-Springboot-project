6ebc788cf19d34e6a9c67c7e301d53e828c3cac4
Shashidharak89 <shashidharak334@gmail.com>
2025-11-12 21:59:12 +0530

    initial commit

---
+++ Beginner/src/main/java/com/first/Beginner/Hello.java
@@ -0,0 +1,35 @@
+package com.first.Beginner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @Autowired
+    private final Nayi nayi;
+
+    public Hello(){
+        nayi =new Nayi();
+    }
+
+    @GetMapping("/")
+    public String greet(){
+        return "Hello shashi";
+    }
+
+//    @GetMapping("/display")
+//    public String display(){
+//        return "index.html";
+//    }
+
+
+    @GetMapping("/getnayi")
+    public Nayi getnode(){
+        return nayi;
+    }
+
+}
---
+++ Beginner/src/main/java/com/first/Beginner/Nayi.java
@@ -0,0 +1,8 @@
+package com.first.Beginner;
+
+public class Nayi {
+    Long id=101l;
+    String name="Shashi";
+
+    public Nayi(){}
+}
